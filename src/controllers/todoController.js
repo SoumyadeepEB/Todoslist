623eb50a40b3132ca0a5afef28b7d83a3565c4c5
Soumyadeep  Ghosh <matainja079@gmail.com>
2021-07-22 18:50:50 +0530

    api changes

--- src/controllers/todoController.js
+++ src/controllers/todoController.js
@@ -40,122 +40,158 @@ exports.list = (req, res) => {
 }
 
 exports.listone = (req, res) => {
-    Todos.findOne({_id: req.params.id}, (err, data) => {
-        if(!err){
-            if(data){
-                res.status(200).json({
-                    status: 'success',
-                    data: data
-                })
-            }else{
-                res.status(200).json({
-                    status: 'success',
-                    message: 'No record found'
-                })
-            }
-        }else{
-            res.status(400).json({
-                status: 'error',
-                message: 'Something is going wrong'
-            })
-        }
-    })
-}
-
-exports.add = (req, res) => {
-    if(req.body.name === null && req.body.desc === null){
-        res.status(400).json({
-            status: 'error',
-            message: 'Todo name or description cannot be empty'
-        })
-    }else{
-        const todo = new Todos({
-            name: req.body.name,
-            description: req.body.desc
-        })
-        todo.save((err, data) => {
+    if(req.headers && req.headers.authorization == `Bearer ${process.env.TOKEN}`){
+        let id = req.params.id
+        Todos.findOne({_id: id}, (err, data) => {
             if(!err){
-                res.status(201).json({
-                    status: 'success',
-                    message: 'Todo inserted successfully'
-                })
+                if(data){
+                    res.status(200).json({
+                        status: 'success',
+                        data: data
+                    })
+                }else{
+                    res.status(200).json({
+                        status: 'success',
+                        message: 'No record found'
+                    })
+                }
             }else{
-                res.status(500).json({
+                res.status(400).json({
                     status: 'error',
                     message: 'Something is going wrong'
                 })
             }
+        })
+    }else{
+        res.status(401).json({
+            status: 'error',
+            message: 'Authentication failed'
+        })
+    }
+}
+
+exports.add = (req, res) => {
+    if(req.headers && req.headers.authorization == `Bearer ${process.env.TOKEN}`){
+        if(req.body.name === null && req.body.desc === null){
+            res.status(400).json({
+                status: 'error',
+                message: 'Todo name or description cannot be empty'
+            })
+        }else{
+            const todo = new Todos({
+                name: req.body.name,
+                description: req.body.desc
+            })
+            todo.save((err, data) => {
+                if(!err){
+                    res.status(201).json({
+                        status: 'success',
+                        message: 'Todo inserted successfully'
+                    })
+                }else{
+                    res.status(500).json({
+                        status: 'error',
+                        message: 'Something is going wrong'
+                    })
+                }
+            })
+        }
+    }else{
+        res.status(401).json({
+            status: 'error',
+            message: 'Authentication failed'
         })
     }
 }
 
 exports.edit = (req, res) => {
-    const id = req.params.id
-    if(req.body.name === null && req.body.desc === null){
-        res.status(400).json({
-            status: 'error',
-            message: 'Todo name or description cannot be empty'
-        })
+    if(req.headers && req.headers.authorization == `Bearer ${process.env.TOKEN}`){
+        let id = req.params.id
+        if(req.body.name === null && req.body.desc === null){
+            res.status(400).json({
+                status: 'error',
+                message: 'Todo name or description cannot be empty'
+            })
+        }else{
+            Todos.findByIdAndUpdate({_id: id}, {
+                name: req.body.name,
+                description: req.body.desc,
+            }, {returnOriginal: false},
+            (err, data) => {
+                if(!err){
+                    res.status(200).json({
+                        status: 'success',
+                        message: 'Todo updated successfully'
+                    })
+                }else{
+                    res.status(500).json({
+                        status: 'error',
+                        message: 'Something is going wrong'
+                    })
+                }
+            })
+        }
     }else{
-        Todos.findByIdAndUpdate({_id: id}, {
-            name: req.body.name,
-            description: req.body.desc,
-        }, {returnOriginal: false},
-        (err, data) => {
-            if(!err){
-                res.status(200).json({
-                    status: 'success',
-                    message: 'Todo updated successfully'
-                })
-            }else{
-                res.status(500).json({
-                    status: 'error',
-                    message: 'Something is going wrong'
-                })
-            }
+        res.status(401).json({
+            status: 'error',
+            message: 'Authentication failed'
         })
-    } 
+    }
 }
 
 exports.change = (req, res) => {
-    const id = req.params.id
-    if(req.body.status === null || isNaN(req.body.status)){
-        res.status(500).json({
-            status: 'error',
-            message: 'Status should be numeric'
-        })
+    if(req.headers && req.headers.authorization == `Bearer ${process.env.TOKEN}`){
+        let id = req.params.id
+        if(req.body.status === null || isNaN(req.body.status)){
+            res.status(500).json({
+                status: 'error',
+                message: 'Status should be numeric'
+            })
+        }else{
+            let status = req.body.status == 1 ? true : false
+            Todos.findByIdAndUpdate({_id: id}, {status: status}, {returnOriginal: false},
+            (err, data) => {
+                if(!err){
+                    res.status(200).json({
+                        status: 'success',
+                        message: 'Todo status changed'
+                    })
+                }else{
+                    res.status(500).json({
+                        status: 'error',
+                        message: 'Something is going wrong'
+                    })
+                }
+            })
+        }
     }else{
-        let status = req.body.status == 1 ? true : false
-        Todos.findByIdAndUpdate({_id: id}, {status: status}, {returnOriginal: false},
-        (err, data) => {
-            if(!err){
-                res.status(200).json({
-                    status: 'success',
-                    message: 'Todo status changed'
-                })
-            }else{
-                res.status(500).json({
-                    status: 'error',
-                    message: 'Something is going wrong'
-                })
-            }
+        res.status(401).json({
+            status: 'error',
+            message: 'Authentication failed'
         })
     }
 }
 
 exports.delete = (req, res) => {
-    let id = req.params.id
-    Todos.findOneAndDelete({_id: id}, (err, data) => {
-        if(!err){
-            res.status(200).json({
-                status: 'success',
-                message: 'Todo deleted successfully'
-            })
-        }else{
-            res.status(500).json({
-                status: 'error',
-                message: 'Something is going wrong'
-            })
-        }
-    })
+    if(req.headers && req.headers.authorization == `Bearer ${process.env.TOKEN}`){
+        let id = req.params.id
+        Todos.findOneAndDelete({_id: id}, (err, data) => {
+            if(!err){
+                res.status(200).json({
+                    status: 'success',
+                    message: 'Todo deleted successfully'
+                })
+            }else{
+                res.status(500).json({
+                    status: 'error',
+                    message: 'Something is going wrong'
+                })
+            }
+        })
+    }else{
+        res.status(401).json({
+            status: 'error',
+            message: 'Authentication failed'
+        })
+    }
 }
